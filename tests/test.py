2368f6f7b1bad90ea4744dc6644a13aca5b83c85
BurhanH <urazalinov@gmail.com>
2019-01-30 20:57:47 -0600

    Improvements for tests

--- tests/test.py
+++ tests/test.py
@@ -7,14 +7,19 @@ from queries import Query
 
 
 class APITestCase(TestCase):
-    def test_api_1(self):
+    def _test(self, schema_name, argument=''):
         client = Client(graphene.Schema(query=Query))
-        self.assert_match_snapshot(client.execute('''{ hello }'''))
+        if argument is not '':
+            line = schema_name + ' (argument: "{0}")'.format(argument)
+        else:
+            line = schema_name
+        self.assert_match_snapshot(client.execute('''{ ''' + line + ''' }'''))
+
+    def test_api_1(self):
+        self._test('hello')
 
     def test_api_2(self):
-        client = Client(graphene.Schema(query=Query))
-        self.assert_match_snapshot(client.execute('''{ hello (argument: "graph") }'''))
+        self._test('hello', 'graph')
 
     def test_api_3(self):
-        client = Client(graphene.Schema(query=Query))
-        self.assert_match_snapshot(client.execute('''{ hello (argument: "world") }'''))
+        self._test('hello', 'world')
