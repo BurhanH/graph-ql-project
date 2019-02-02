1d467ce5ec2f6cfb42de0825d4e8920231a89852
Baur Urazalinov <BurhanH@users.noreply.github.com>
2019-02-01 21:09:24 -0600

    Update queries.py

--- queries.py
+++ queries.py
@@ -4,6 +4,5 @@ import graphene
 class Query(graphene.ObjectType):
     hello = graphene.String(argument=graphene.String(default_value="stranger"))
     
-    @classmethod
     def resolve_hello(self, info, argument):
         return 'Hello ' + argument
